b8fa961836fcfbc8dda9cef47f11b4da2e3b4b26
nattan junior <fnatanieljunior@gmail.com>
2026-04-17 14:17:30 -0300

    feat: add PostgreSQL setup with Prisma and create initial database migrations

---
+++ prisma/migrations/20260417165837_create_gyms_end_check_ins/migration.sql
@@ -0,0 +1,34 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `password` on the `users` table. All the data in the column will be lost.
+  - Added the required column `password_hash` to the `users` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `users` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "password",
+ADD COLUMN     "password_hash" TEXT NOT NULL,
+ALTER COLUMN "name" SET NOT NULL;
+
+-- CreateTable
+CREATE TABLE "check_ins" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "validetedAt" TIMESTAMP(3),
+
+    CONSTRAINT "check_ins_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "gyms" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "phone" TEXT,
+    "latitude" DOUBLE PRECISION NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "gyms_pkey" PRIMARY KEY ("id")
+);
